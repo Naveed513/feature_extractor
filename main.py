7ffa2a90843fa06228867b77365d71e4c36caae8
Naveed <101715691+Naveed513@users.noreply.github.com>
2023-02-25 10:41:24 +0530

    Update main.py
    The reason for this commit is to implement multiprocessing in order to improve the efficiency of the program.

--- main.py
+++ main.py
@@ -2,20 +2,26 @@
 """
 Created on Fri Feb 24 14:44:22 2023
 
-@author: SPSOFT
+@author: Naveed
 """
-
-from feature_extractor_functions import FeatureExtractor 
-
-
-
+from feature_extractor_functions import FeatureExtractor
+from multiprocess import Pool
+import os
+import multiprocessing
+print('No.of cores available in system is:', multiprocessing.cpu_count()) # checking no.of cores available in system 
 
 if __name__ == '__main__':
-    files_path = 'D:/poll/finall_poll/test1' # path where input pdf files are stored
-    img_path = "D:/poll/finall_poll/temp_image" # path to temporarily store images will be deleted after programme complition
-    result_path = 'D:/poll/finall_poll/extracted_result' # path where results need to be stored
-    block_model_path = "D:/poll/finall_poll/block.pt" # block extractor yolo model path
-    feature_model_path = "D:/poll/finall_poll/feature.pt" # feature extractor yolo model path
+    files_path = "test" # path where input pdf files are stored
+    img_path = "temp_img" # path to temporarily store images will be deleted after programme complition
+    result_path = 'extracted_result' # path where results need to be stored
+    block_model_path = "yolov5_models/block.pt" # block extractor yolo model path
+    feature_model_path = "yolov5_models/feature.pt" # feature extractor yolo model path
     extractor = FeatureExtractor(files_path, img_path, result_path, block_model_path, feature_model_path)
-    extractor.model_init() # initializing yolov5 models
-    extractor.data_extractor() # runing extractor
+    files_list = os.listdir(files_path) # list of pdf files
+    filenames = [(sleep_value, file) for sleep_value, file in zip(range(0, len(files_list)*3, 3), files_list) 
+                 if os.path.splitext(file)[-1].lower() == '.pdf']
+    # starting multiprocessing 
+    pool_process = Pool(12) # the available cores in my system is 12
+    pool_process.map(extractor.data_extractor, filenames)
+    pool_process.close()
+    pool_process.join()
